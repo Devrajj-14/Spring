41b431f982b59ef3304125422a955e2f1c08535a
23f3000161 <23f3000161@ds.study.iitm.ac.in>
2026-03-22 10:32:45 +0530

    [Devraj]: add query parameter endpoint for uc2

--- src/main/java/com/bridgelabz/spring/controller/HelloController.java
+++ src/main/java/com/bridgelabz/spring/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    // UC2: GET /hello/query?name=... - returns hello with name from query parameter
+    @GetMapping("/hello/query")
+    public String sayHelloWithQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
