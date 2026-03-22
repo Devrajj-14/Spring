ef9abbebcdf48780773b4e7423372d0dee7cb502
23f3000161 <23f3000161@ds.study.iitm.ac.in>
2026-03-22 10:30:49 +0530

    [Devraj]: initial spring boot project setup

---
+++ src/main/java/com/bridgelabz/spring/SpringApp.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(SpringApp.class, args);
+	}
+
+}
